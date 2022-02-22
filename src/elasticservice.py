3042502c7e3c0bb6e0c0276bda4a26fcac3bf3fc
kazizi-dev <kouroshazizi2012@gmail.com>
2022-02-22 00:31:09 -0800

    enhance elastic parameters

--- src/elasticservice.py
+++ src/elasticservice.py
@@ -1,10 +1,4 @@
-from datetime import datetime
-from urllib import request
-from elasticsearch import Elasticsearch
-import certifi
 import config
-from colorama import Fore, Style
-import json
 import requests
 
 class ElasticService():
@@ -18,33 +12,28 @@ class ElasticService():
 
     
     # check if index name is valid
-    def does_index_exist(self, index):
-        headers = {'Accept': 'application/json', 'Content-type': 'application/json'}
+    def __does_index_exist(self, index):
         response = requests.get(
             self.client_url + '/_cat/indices', 
             data="", 
             auth=(config.ELASTIC_USER, config.ELASTIC_PWD), 
             verify=True, 
-            headers = headers)
+            headers = {'Accept': 'application/json', 'Content-type': 'application/json'}
+        )
 
-        if index in response.text:
-            print(f"{Fore.GREEN}[Elasticsearch]: index found{Style.RESET_ALL}")
-            return True
-        else:
-            print(f"{Fore.RED}[Elasticsearch]: index not found{Style.RESET_ALL}")
-            return False
+        return True if index in response.text else False
         
 
     # query data from elasticsearch index
     def get_query_response(self, index, query):
-        if self.does_index_exist(index):
-            headers={'Accept': 'application/json', 'Content-type': 'application/json'}        
+        if self.__does_index_exist(index):
             response = requests.get(
                 f"{self.client_url}/{index}/_search", 
-                data='{"query": {"match_all": {}}}', 
+                data=query,
                 auth=(config.ELASTIC_USER, config.ELASTIC_PWD), 
                 verify=True, 
-                headers = headers)
+                headers={'Accept': 'application/json', 'Content-type': 'application/json'}
+            )
 
             return response.json()
         
